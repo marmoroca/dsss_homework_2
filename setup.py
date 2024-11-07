e44a93d23334e57560629658fc6f8829d4da4233
María del Mar Moreno <mmoreno@ingeniova.com>
2024-11-07 19:54:38 +0100

    update

--- setup.py
+++ setup.py
@@ -3,6 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name="math_quiz",
     version="0.1",
+    description="A simple math quiz game from Homework 2 of Data Science for Survival Skills ",
+    author="Maria del Mar Moreno",
+    author_email="mar.moreno@alumnos.upm.es",
+    packages=find_packages(),
+    install_requires=[
+        # dependencies from requirements.txt
+    ]
+    
+)
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
     description="A simple math quiz game from Homework 2 of Data Science for Survival Skills",
     author="Maria del Mar Moreno",
     author_email="mar.moreno@alumnos.upm.es",
